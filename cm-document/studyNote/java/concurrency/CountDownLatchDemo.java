1f5923aa40a8097cc355b7fcf83aaad0f4eb7014
miao.chen01 <miao.chen01@hand-china.com>
2020-03-29 11:55:20 +0800

    [ADD]信号量 CountDownLatch

---
+++ cm-document/studyNote/java/concurrency/CountDownLatchDemo.java
@@ -0,0 +1,40 @@
+package concurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 锁存器
+ *
+ * @author parkstud@qq.com 2020-03-28
+ */
+@Slf4j
+public class CountDownLatchDemo {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        log.info("Starting.....");
+        new MyThread(countDownLatch);
+        countDownLatch.await();
+        log.info("Done");
+
+    }
+}
+
+@Slf4j
+class MyThread implements Runnable {
+    CountDownLatch latch;
+
+    public MyThread(CountDownLatch latch) {
+        this.latch = latch;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            log.info("i:{}", i);
+            latch.countDown();
+        }
+    }
+}
